dd5b3eeb8ea8cb51cad87622a36d243757cbb3ae
Nilo Junio Nunes Farias Trigueiro <nnfarias@recordbrasilia.com.br>
2025-06-23 14:15:55 -0300

    Removendo todos os arquivos da master

--- 2 - VMs/Processadores Vm_contagemVCPu/hyperv_cpu_count.ps1
+++
@@ -1,9 +0,0 @@
-﻿# Obtém todas as VMs no Hyper-V e percorre cada uma delas
-Get-VM | ForEach-Object {
-    # Para cada VM, exibe uma string com o nome da VM seguido do número de vCPUs atribuídas
-    "$($_.Name):$($_.ProcessorCount)"
-}
-
-
-###Função : Exibir no terminal uma lista com o nome de cada VM do Hyper-V seguido pela quantidade de processadores virtuais (vCPUs) atribuídos a ela.
-### Uso: Fazer um inventário rápido dos recursos de CPU atribuídos às VMs.
